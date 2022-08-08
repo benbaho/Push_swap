2883a458a7b8fb54dc038f3bfa557c8c6ec57957
benbaho <mrbahadir25@gmail.com>
2022-08-08 10:29:32 +0300

    new

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-# Push_swap
-=======
 # Push_swap
 
 
 Hello world.
 
->>>>>>> 33c4f9fee81162d96c054773bc686e7a0606196d
--- swaps.c
+++ swaps.c
@@ -1,63 +1,12 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   swaps.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bdurmus <bdurmus@student.42kocaeli.co      +#+  +:+       +#+        */
+/*   By: bdurmus <bdurmus@student.42kocaeli.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/29 10:53:01 by bdurmus           #+#    #+#             */
-/*   Updated: 2022/07/29 11:27:39 by bdurmus          ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "push_swap.h"
-
-static t_stk *sx(t_stk *list)
-{
-	if (list && !(list->next))
-		return (list);
-	t_stk	*keep;
-	t_stk	*tmp;
-
-	keep = list->next;
-	tmp = keep->next;
-	keep->next = list;
-	list->next = tmp;
-	return (keep);
-}
-
-void	sa(t_stk **a)
-{
-	*a = sx(*a);
-	ft_printf("sa\n");
-}
-
-void 	sb(t_stk **b)
-{
-	*b = sx(*b);
-	ft_printf("sb\n");
-}
-
-void	ss(t_stk **a, t_stk **b)
-{
-	*a = sx(*a);
-	*b = sx(*b);
-	ft_printf("ss\n");
-}
-
-
-
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   swaps.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bdurmus <bdurmus@student.42kocaeli.co      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/29 10:53:01 by bdurmus           #+#    #+#             */
-/*   Updated: 2022/07/29 11:27:39 by bdurmus          ###   ########.tr       */
+/*   Updated: 2022/08/08 10:28:20 by bdurmus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,4 +47,3 @@ void	ss(t_stk **a, t_stk **b)
 
 
 
->>>>>>> 33c4f9fee81162d96c054773bc686e7a0606196d
